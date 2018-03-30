7ecd1aa893dba25616c2d5a81307eee31effe19e
Marlon <martianworm17@gmail.com>
2018-03-30 21:30:31 +0800

    Code Demo - Rough Prototype Part 1

--- src/com/kamotelabs/springdemo/BaseballCoach.java
+++ src/com/kamotelabs/springdemo/BaseballCoach.java
@@ -1,7 +1,8 @@
 package com.kamotelabs.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
 	
+	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice.";
 	}
---
+++ src/com/kamotelabs/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.kamotelabs.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+
+}
